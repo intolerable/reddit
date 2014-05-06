19a8bbc5dd25feec320cf54a5f3cb7e888021589
Fraser Murray <fraser.m.murray@gmail.com>
2014-05-06 11:49:19 +0100

    made bot functions a bit more correct

--- src/Reddit/Bot.hs
+++ src/Reddit/Bot.hs
@@ -15,30 +15,38 @@ instance Monoid Bot where
   mappend (Bot b1) (Bot b2) = Bot (b1 >> b2)
   mempty = Bot (return ())
 
-runBots :: [Bot] -> Text -> Text -> IO (Either (APIError RedditError) ())
-runBots bs user pass = runReddit user pass $ unBot $ mconcat bs
+runBots :: [Bot] -> Text -> Text -> IO ()
+runBots bs user pass = void $ runReddit user pass $ unBot $ mconcat bs
 
 every :: Int -> Reddit a -> Reddit ()
 every n act = forever $ void act >> wait n
 
+-- | Rate limit a @Reddit@ action, waiting for 60 seconds if the rate limit is reached.
+--
+-- > rateLimit = rateLimitWith (wait 60)
 rateLimit :: Reddit a -> Reddit a
-rateLimit = rateLimitEvery 60 
+rateLimit = rateLimitWith (wait 60) 
 
 forkReddit :: Reddit a -> Reddit ()
 forkReddit a = do
   io <- makeIO a
   void $ liftIO $ forkIO $ void io
 
-rateLimitEvery :: Int -> Reddit a -> Reddit a
-rateLimitEvery duration act = do
+-- | Rate limit an action using a specified action. If the rate limit is reached when 
+--   running the second action, the first action is executed, then the first action is run
+--   again.
+rateLimitWith :: Reddit () -> Reddit a -> Reddit a
+rateLimitWith onLimit act = do
   res <- nest act
   case res of 
     Left (APIError (RateLimitError msg)) -> do
       liftIO $ print msg
-      wait duration
+      onLimit
       rateLimit act
     Left x -> EitherT . return $ Left x
     Right x -> return x
 
+-- | Wait @n@ seconds. Essentially @threadDelay@ in the @Reddit@ type using seconds
+--   instead of microseconds.
 wait :: Int -> Reddit ()
 wait = liftIO . threadDelay . (* 1000000)
