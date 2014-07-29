f656082bd487190da675237c286f5d3801ab0a0e
Fraser Murray <fraser.m.murray@gmail.com>
2014-07-29 21:49:29 +0100

    escape wiki page stuff, fixed #7

--- src/Reddit/API/Types/Wiki.hs
+++ src/Reddit/API/Types/Wiki.hs
@@ -9,6 +9,7 @@ import Data.DateTime (DateTime)
 import Data.Monoid (mempty)
 import Data.Text (Text)
 import qualified Data.DateTime as DateTime
+import qualified Data.Text as Text
 
 
 newtype RevisionID = RevisionID Text
@@ -21,12 +22,16 @@ data WikiPage = WikiPage { contentHTML :: Text
                          , canRevise :: Bool }
   deriving (Show, Read, Eq)
 
+unescape :: Text -> Text
+unescape = replace "&gt;" ">" . replace "&lt;" "<" . replace "&amp;" "&"
+  where replace s r = Text.intercalate r . Text.splitOn s
+
 instance FromJSON WikiPage where
   parseJSON (Object o) = do
     o `ensureKind` "wikipage"
     d <- o .: "data"
-    WikiPage <$> d .: "content_html"
-             <*> d .: "content_md"
+    WikiPage <$> (unescape <$> d .: "content_html")
+             <*> (unescape <$> d .: "content_md")
              <*> (DateTime.fromSeconds <$> d .: "revision_date")
              <*> ((d .: "revision_by") >>= (.: "data") >>= (.: "name"))
              <*> d .: "may_revise"
