87cedcf9c0de87da68ceacadaa2011ecb7bc33f3
Fraser Murray <fraser.m.murray@gmail.com>
2014-09-17 16:05:44 +0100

    explicit imports and neater ensureKind

--- src/Reddit/API/Parser.hs
+++ src/Reddit/API/Parser.hs
@@ -2,10 +2,12 @@ module Reddit.API.Parser
   ( decode
   , ensureKind ) where
 
-import Control.Monad
-import Data.Aeson.Types
+import Control.Monad (guard)
+import Data.Aeson.Types (Parser, Object, (.:))
 import Data.Text (Text)
-import Network.API.Builder
+import Network.API.Builder (decode)
 
 ensureKind :: Object -> Text -> Parser ()
-ensureKind o k = (o .: "kind") >>= guard . (== k) >> return ()
+ensureKind o k = do
+  kind <- o .: "kind"
+  guard $ kind == k
