40e531b88de7f42ec23dfd7682021c9cf0ef0042
Fraser Murray <fraser.m.murray@gmail.com>
2014-09-19 19:20:04 +0100

    handle rate limit issues on login

--- src/Reddit/API/Login.hs
+++ src/Reddit/API/Login.hs
@@ -1,12 +1,15 @@
 module Reddit.API.Login
-  ( module Reddit.API.Login ) where
+  ( login ) where
 
+import Reddit.API.Types.Error
 import Reddit.API.Types.Reddit
 
+import Control.Concurrent (threadDelay)
 import Control.Exception (try)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Either
 import Control.Monad.Trans.State
+import Data.Bifunctor (first)
 import Data.Text (Text)
 import Network.API.Builder
 import Network.HTTP.Conduit
@@ -20,15 +23,39 @@ loginRoute user pass = Route [ "api", "login" ]
 
 getLoginDetails :: MonadIO m => Text -> Text -> RedditT m LoginDetails
 getLoginDetails user pass = do
+  (limiting, _) <- RedditT $ liftState get
+  if limiting
+    then getLoginDetailsWithLimiting user pass
+    else getLoginDetails' user pass
+
+getLoginDetails' :: MonadIO m => Text -> Text -> RedditT m LoginDetails
+getLoginDetails' user pass = do
   b <- RedditT $ liftBuilder get
-  req <- RedditT . EitherT . return $ case routeRequest b (loginRoute user pass) of
+  req <- RedditT $ hoistEither $ case routeRequest b (loginRoute user pass) of
     Just url -> Right url
     Nothing -> Left InvalidURLError
   resp <- liftIO $ try $ withManager $ httpLbs req
-  resp' <- RedditT $ hoistEither $ either (Left . HTTPError) Right resp
+  resp' <- RedditT $ hoistEither $ first HTTPError resp
   let cj = responseCookieJar resp'
-  mh <- RedditT . EitherT . return . decode $ responseBody resp'
+  mh <- RedditT $ hoistEither $ decode $ responseBody resp'
   return $ LoginDetails mh cj
+
+getLoginDetailsWithLimiting :: MonadIO m => Text -> Text -> RedditT m LoginDetails
+getLoginDetailsWithLimiting user pass = do
+  b <- RedditT $ liftBuilder get
+  req <- RedditT $ hoistEither $ case routeRequest b (loginRoute user pass) of
+    Just url -> Right url
+    Nothing -> Left InvalidURLError
+  resp <- liftIO $ try $ withManager $ httpLbs req
+  resp' <- RedditT $ hoistEither $ first HTTPError resp
+  let cj = responseCookieJar resp'
+  mh <- nest $ RedditT $ hoistEither $ decode $ responseBody resp'
+  case mh of
+    Left (APIError (RateLimitError wait _)) -> do
+      liftIO $ threadDelay $ ((fromIntegral wait) + 5) * 1000000
+      getLoginDetailsWithLimiting user pass
+    Left x -> RedditT $ hoistEither $ Left x
+    Right modhash -> return $ LoginDetails modhash cj
 
 login :: MonadIO m => Text -> Text -> RedditT m LoginDetails
 login user pass = do
