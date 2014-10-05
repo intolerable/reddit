b6d5a72d4dae61d09d12179a9606e14f076eba3d
Fraser Murray <fraser.m.murray@gmail.com>
2014-10-05 21:48:36 +0100

    hlint fixes and renaming qualified imports

--- src/Reddit/API/Actions/Captcha.hs
+++ src/Reddit/API/Actions/Captcha.hs
@@ -3,12 +3,12 @@ module Reddit.API.Actions.Captcha where
 import Reddit.API.Routes.Run
 import Reddit.API.Types.Captcha
 import Reddit.API.Types.Reddit
-import qualified Reddit.API.Routes.Captcha as Routes
+import qualified Reddit.API.Routes.Captcha as Route
 
 import Control.Monad.IO.Class
 
 needsCaptcha :: MonadIO m => RedditT m Bool
-needsCaptcha = runRoute $ Routes.needsCaptcha
+needsCaptcha = runRoute Route.needsCaptcha
 
 newCaptcha :: MonadIO m => RedditT m CaptchaID
-newCaptcha = runRoute $ Routes.newCaptcha
+newCaptcha = runRoute Route.newCaptcha
