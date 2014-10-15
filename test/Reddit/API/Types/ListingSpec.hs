2dfe545625f88ae0ccf22779aa4fbe6d528f637a
Fraser Murray <fraser.m.murray@gmail.com>
2014-10-15 18:35:35 +0100

    enforce some invariants on Listing

--- test/Reddit/API/Types/ListingSpec.hs
+++ test/Reddit/API/Types/ListingSpec.hs
@@ -7,6 +7,7 @@ import Control.Monad
 import Data.Either
 import Network.API.Builder
 import Test.Hspec
+import Test.Hspec.QuickCheck
 import qualified Data.ByteString.Lazy.Char8 as ByteString
 
 main :: IO ()
@@ -14,28 +15,46 @@ main = hspec spec
 
 spec :: Spec
 spec = describe "Reddit.API.Types.Listing" $ do
-  getUserCommentsExample <- runIO $ ByteString.readFile "test/data/getUserComments_example.json"
+  describe "Listing" $ do
+    getUserCommentsExample <- runIO $ ByteString.readFile "test/data/getUserComments_example.json"
 
-  it "can read the example" $
-    getUserCommentsExample `shouldSatisfy` not . ByteString.null
+    it "can read the example" $
+      getUserCommentsExample `shouldSatisfy` not . ByteString.null
 
-  it "can parse a listing from json" $ do
-    let decoded = decode getUserCommentsExample :: Either (APIError ()) CommentListing
-    decoded `shouldSatisfy` isRight
+    it "can parse a listing from json" $ do
+      let decoded = decode getUserCommentsExample :: Either (APIError ()) CommentListing
+      decoded `shouldSatisfy` isRight
 
-    case decoded of
-      Left _ -> expectationFailure "json parse failed"
-      Right l@(Listing b a cs) -> do
-        a `shouldBe` Just (CommentID "t1_cl1royq")
-        b `shouldBe` Nothing
-        length cs `shouldBe` length (contents (void l))
+      case decoded of
+        Left _ -> expectationFailure "json parse failed"
+        Right (Listing b a _) -> do
+          a `shouldBe` Just (CommentID "t1_cl1royq")
+          b `shouldBe` Nothing
 
-  it "can parse listings from empty strings" $ do
-    let decoded :: Either (APIError ()) CommentListing
-        decoded = decode "\"\""
-    decoded `shouldBe` Right (Listing Nothing Nothing [])
+    describe "has a valid Functor instance" $ do
+      prop "length l = length (fmap f l)" $ \xs ->
+        let l = Listing Nothing Nothing (xs :: [()]) in
+        length (contents (l :: Listing () ())) == length (contents (void l))
 
-  it "can parse listings from null" $ do
-    let decoded :: Either (APIError ()) CommentListing
-        decoded = decode "null"
-    decoded `shouldBe` Right (Listing Nothing Nothing [])
+      prop "fmap id x == x" $ \xs ->
+        let l = Listing Nothing Nothing (xs :: [String]) in
+        fmap id l == (l :: Listing () String)
+
+    it "can parse listings from empty strings" $ do
+      let decoded :: Either (APIError ()) CommentListing
+          decoded = decode "\"\""
+      decoded `shouldBe` Right (Listing Nothing Nothing [])
+
+    it "can parse listings from null" $ do
+      let decoded :: Either (APIError ()) CommentListing
+          decoded = decode "null"
+      decoded `shouldBe` Right (Listing Nothing Nothing [])
+
+  describe "ListingType" $ do
+
+    it "should have a valid ToQuery instance" $ do
+      toQuery Hot `shouldBe` Just "hot"
+      toQuery New `shouldBe` Just "new"
+      toQuery Rising `shouldBe` Just "rising"
+      toQuery Controversial `shouldBe` Just "controversial"
+      toQuery Top `shouldBe` Just "top"
