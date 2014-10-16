5ff9ee73c9ea7c15146b135b25039574bdd7a09f
Fraser Murray <fraser.m.murray@gmail.com>
2014-10-16 16:29:56 +0100

    basic test for unescape

---
+++ test/Reddit/API/Types/WikiSpec.hs
@@ -0,0 +1,17 @@
+module Reddit.API.Types.WikiSpec where
+
+import Reddit.API.Types.Wiki
+
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = describe "Reddit.API.Types.Wiki" $ do
+
+  it "should correctly unescape text" $ do
+    unescape "&gt;" `shouldBe` ">"
+    unescape "&lt;" `shouldBe` "<"
+    unescape "&amp;" `shouldBe` "&"
+    unescape "&lt;br/&gt;" `shouldBe` "<br/>"
