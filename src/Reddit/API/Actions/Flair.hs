45efcc9572a13acfd59bb1aa4eba8063d2f97cfc
Fraser Murray <fraser.m.murray@gmail.com>
2014-10-16 11:29:47 +0100

    add options for getFlairList

--- src/Reddit/API/Actions/Flair.hs
+++ src/Reddit/API/Actions/Flair.hs
@@ -3,11 +3,16 @@ module Reddit.API.Actions.Flair where
 import Reddit.API.Routes.Flair
 import Reddit.API.Routes.Run
 import Reddit.API.Types.Flair
+import Reddit.API.Types.Options
 import Reddit.API.Types.Reddit
 import Reddit.API.Types.Subreddit
 
 import Control.Monad.IO.Class
+import Data.Default
 import Data.Text (Text)
 
-getFlairList :: MonadIO m => Maybe Text -> Maybe Text -> Maybe Int -> SubredditName -> RedditT m FlairList
-getFlairList b a l r = runRoute $ flairList b a l r
+getFlairList :: MonadIO m => SubredditName -> RedditT m FlairList
+getFlairList = getFlairList' def
+
+getFlairList' :: MonadIO m => Options Text -> SubredditName -> RedditT m FlairList
+getFlairList' opts r = runRoute $ flairList opts r
--- src/Reddit/API/Routes/Flair.hs
+++ src/Reddit/API/Routes/Flair.hs
@@ -1,14 +1,15 @@
 module Reddit.API.Routes.Flair where
 
+import Reddit.API.Types.Options
 import Reddit.API.Types.Subreddit
 
 import Data.Text (Text)
 import Network.API.Builder.Routes
 
-flairList :: Maybe Text -> Maybe Text -> Maybe Int -> SubredditName -> Route
-flairList b a l (R r) =
+flairList :: Options Text -> SubredditName -> Route
+flairList opts (R r) =
   Route [ "r", r, "api", "flairlist" ]
-        [ "after" =. a
-        , "before" =. b
-        , "limit" =. l ]
+        [ "after" =. after opts
+        , "before" =. before opts
+        , "limit" =. limit opts ]
         "GET"
