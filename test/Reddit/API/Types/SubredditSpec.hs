acf4af6ff665452f18c9513a7db3017102e7f412
Fraser Murray <fraser.m.murray@gmail.com>
2014-10-21 11:19:11 +0100

    formatting

--- test/Reddit/API/Types/SubredditSpec.hs
+++ test/Reddit/API/Types/SubredditSpec.hs
@@ -1,9 +1,10 @@
 module Reddit.API.Types.SubredditSpec where
 
+import Reddit.API.Types.Subreddit
+
 import Data.ByteString.Lazy (ByteString)
 import Data.Either
 import Network.API.Builder
-import Reddit.API.Types.Subreddit
 import Test.Hspec
 
 main :: IO ()
@@ -13,7 +14,7 @@ spec :: Spec
 spec = describe "Reddit.API.Types.Subreddit" $ do
   let decode' = decode :: ByteString -> Either (APIError ()) SubredditID
 
-  it "should be able to parse a SubredditID" $ do
+  it "can parse a SubredditID" $ do
     decode' "\"t5_2s580\"" `shouldBe` Right (SubredditID "2s580")
     decode' "\"2s580\"" `shouldBe` Right (SubredditID "2s580")
     decode' "\"t1_cl1royq\"" `shouldSatisfy` isLeft
