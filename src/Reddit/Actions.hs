46ff235c2aaed7a2cf1ae230cb60607a449aa619
Fraser Murray <fraser.m.murray@gmail.com>
2015-07-19 22:47:48 +0100

    explicit exports because haddock is ass

--- src/Reddit/Actions.hs
+++ src/Reddit/Actions.hs
@@ -1,16 +1,27 @@
 -- | All the actions that the library can perform.
 module Reddit.Actions
-  ( module Actions ) where
+  ( module Reddit.Actions.Captcha
+  , module Reddit.Actions.Comment
+  , module Reddit.Actions.Flair
+  , module Reddit.Actions.Message
+  , module Reddit.Actions.Moderation
+  , module Reddit.Actions.Post
+  , module Reddit.Actions.Search
+  , module Reddit.Actions.Subreddit
+  , module Reddit.Actions.Thing
+  , module Reddit.Actions.User
+  , module Reddit.Actions.Voting
+  , module Reddit.Actions.Wiki ) where
 
-import Reddit.Actions.Captcha as Actions
-import Reddit.Actions.Comment as Actions
-import Reddit.Actions.Flair as Actions
-import Reddit.Actions.Message as Actions
-import Reddit.Actions.Moderation as Actions
-import Reddit.Actions.Post as Actions
-import Reddit.Actions.Search as Actions
-import Reddit.Actions.Subreddit as Actions
-import Reddit.Actions.Thing as Actions
-import Reddit.Actions.User as Actions
-import Reddit.Actions.Voting as Actions
-import Reddit.Actions.Wiki as Actions
+import Reddit.Actions.Captcha
+import Reddit.Actions.Comment
+import Reddit.Actions.Flair
+import Reddit.Actions.Message
+import Reddit.Actions.Moderation
+import Reddit.Actions.Post
+import Reddit.Actions.Search
+import Reddit.Actions.Subreddit
+import Reddit.Actions.Thing
+import Reddit.Actions.User
+import Reddit.Actions.Voting
+import Reddit.Actions.Wiki
