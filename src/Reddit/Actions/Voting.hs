e5508adf9cab89e0d988bf66c48f0426686c0c83
Fraser Murray <fraser.m.murray@gmail.com>
2015-07-21 19:11:09 +0100

    make an ADT for voting

--- src/Reddit/Actions/Voting.hs
+++ src/Reddit/Actions/Voting.hs
@@ -7,35 +7,36 @@ module Reddit.Actions.Voting
   , unvoteComment ) where
 
 import Reddit.Routes.Run
+import Reddit.Routes.Vote (VoteDirection(..))
 import Reddit.Types
 import Reddit.Types.Empty
 import qualified Reddit.Routes as Route
 
 import Control.Monad.IO.Class
 
-vote :: (MonadIO m, Thing a) => Int -> a -> RedditT m ()
+vote :: (MonadIO m, Thing a) => VoteDirection -> a -> RedditT m ()
 vote dir = nothing . runRoute . Route.vote dir
 
 -- | Upvote a post.
 upvotePost :: MonadIO m => PostID -> RedditT m ()
-upvotePost = vote 1
+upvotePost = vote UpVote
 
 -- | Downvote a post.
 downvotePost :: MonadIO m => PostID -> RedditT m ()
-downvotePost = vote (-1)
+downvotePost = vote DownVote
 
 -- | Remove a vote from a post.
 unvotePost :: MonadIO m => PostID -> RedditT m ()
-unvotePost = vote 0
+unvotePost = vote RemoveVote
 
 -- | Upvote a comment.
 upvoteComment :: MonadIO m => CommentID -> RedditT m ()
-upvoteComment = vote 1
+upvoteComment = vote UpVote
 
 -- | Downvote a comment.
 downvoteComment :: MonadIO m => CommentID -> RedditT m ()
-downvoteComment = vote (-1)
+downvoteComment = vote RemoveVote
 
 -- | Remove a previously-cast vote from a comment.
 unvoteComment :: MonadIO m => CommentID -> RedditT m ()
-unvoteComment = vote 0
+unvoteComment = vote DownVote
--- src/Reddit/Routes/Vote.hs
+++ src/Reddit/Routes/Vote.hs
@@ -2,11 +2,21 @@ module Reddit.Routes.Vote where
 
 import Reddit.Types.Thing
 
+import Network.API.Builder.Query
 import Network.API.Builder.Routes
-import qualified Data.Text as T
 
-vote :: Thing a => Int -> a -> Route
+data VoteDirection = UpVote
+                   | RemoveVote
+                   | DownVote
+  deriving (Show, Read, Eq)
+
+instance ToQuery VoteDirection where
+  toQuery k UpVote = [(k, "1")]
+  toQuery k RemoveVote = [(k, "0")]
+  toQuery k DownVote = [(k, "-1")]
+
+vote :: Thing a => VoteDirection -> a -> Route
 vote direction tID = Route [ "api", "vote" ]
                            [ "id" =. Just (fullName tID)
-                           , "dir" =. Just (T.pack $ show direction) ]
+                           , "dir" =. direction ]
                            "POST"
