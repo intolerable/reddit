93a53152e7d918f79c0e35d0da63a46aa37a3131
Fraser Murray <fraser.m.murray@gmail.com>
2015-07-21 20:51:29 +0100

    un-munge for haddock

--- src/Reddit/Types.hs
+++ src/Reddit/Types.hs
@@ -1,14 +1,37 @@
 module Reddit.Types
-  ( module Types ) where
+  ( CaptchaID(..)
+  , Comment
+  , CommentID(..)
+  , CommentListing
+  , Listing(..)
+  , LoginDetails
+  , Message
+  , MessageID(..)
+  , MessageKind(..)
+  , Modhash
+  , Options(..)
+  , PaginationOption(..)
+  , Post
+  , PostContent(..)
+  , PostID(..)
+  , PostListing
+  , Reddit
+  , RedditError(..)
+  , RedditT
+  , Subreddit
+  , SubredditName(..)
+  , Thing
+  , User
+  , Username(..) ) where
 
-import Reddit.Types.Captcha as Types (CaptchaID(..))
-import Reddit.Types.Comment as Types (CommentID(..), Comment, CommentListing)
-import Reddit.Types.Error as Types (RedditError(..))
-import Reddit.Types.Listing as Types (Listing(..))
-import Reddit.Types.Message as Types (MessageID(..), Message, MessageKind(..))
-import Reddit.Types.Options as Types (PaginationOption(..), Options(..))
-import Reddit.Types.Post as Types (PostID(..), Post, PostListing, PostContent(..))
-import Reddit.Types.Reddit as Types (Reddit, RedditT, Modhash, LoginDetails)
-import Reddit.Types.Subreddit as Types (SubredditName(..), Subreddit)
-import Reddit.Types.Thing as Types (Thing)
-import Reddit.Types.User as Types (Username(..), User)
+import Reddit.Types.Captcha (CaptchaID(..))
+import Reddit.Types.Comment (CommentID(..), Comment, CommentListing)
+import Reddit.Types.Error (RedditError(..))
+import Reddit.Types.Listing (Listing(..))
+import Reddit.Types.Message (MessageID(..), Message, MessageKind(..))
+import Reddit.Types.Options (PaginationOption(..), Options(..))
+import Reddit.Types.Post (PostID(..), Post, PostListing, PostContent(..))
+import Reddit.Types.Reddit (Reddit, RedditT, Modhash, LoginDetails)
+import Reddit.Types.Subreddit (SubredditName(..), Subreddit)
+import Reddit.Types.Thing (Thing)
+import Reddit.Types.User (Username(..), User)
