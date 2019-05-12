f1a0a37b0aff0e85257d10cf2886b4787b69cf55
Fraser Murray <fraser.m.murray@gmail.com>
2019-05-12 17:29:18 +0100

    support new compilers / libs

--- src/Reddit/Types/Listing.hs
+++ src/Reddit/Types/Listing.hs
@@ -32,9 +32,11 @@ data Listing t a = Listing { before :: Maybe t
 instance Functor (Listing t) where
   fmap f (Listing b a x) = Listing b a (fmap f x)
 
-instance Ord t => Monoid (Listing t a) where
-  mappend (Listing a b cs) (Listing d e fs) =
+instance Ord t => Semigroup (Listing t a) where
+  Listing a b cs <> Listing d e fs =
     Listing (max a d) (min b e) (cs <> fs)
+
+instance Ord t => Monoid (Listing t a) where
   mempty = Listing Nothing Nothing []
 
 instance (FromJSON t, FromJSON a) => FromJSON (Listing t a) where
